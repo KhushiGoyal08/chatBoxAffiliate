9eae9aa88730a78f9201c7eee139cb4c27f1f38c
sushantmahajan85 <sushantmahajan85@gmail.com>
2024-03-15 11:54:13 +0530

    pull

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\internship\affiliatechatboxwithios-master"
+export "FLUTTER_ROOT=C:\Users\susha\Downloads\flutter_windows_3.13.9-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Projects\chatBoxAffiliate"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
